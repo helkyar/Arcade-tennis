80125093d6b1e3026e90883d9fa9935f49571224
helkyar <89964202+helkyar@users.noreply.github.com>
2021-09-23 18:27:41 +0200

    Add Setup

---
+++ Snake/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMcontentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div');
+  const score = document.querySelector('span');
+  const btnStart = document.querySelector('.start');
+});
