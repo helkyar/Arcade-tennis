46b393180a06928f57c7370776eb08c30e27ead7
helkyar <89964202+helkyar@users.noreply.github.com>
2021-09-26 23:33:01 +0200

    Add Keybind and reestart functionality

--- Snake/app.js
+++ Snake/app.js
@@ -1,5 +1,54 @@
 document.addEventListener('DOMcontentLoaded', () => {
   const squares = document.querySelectorAll('.grid div');
-  const score = document.querySelector('span');
+  const scoreDisplay = document.querySelector('span');
   const btnStart = document.querySelector('.start');
+
+  const width = 10;
+  let index = 0;
+  let appleIndex = 0;
+  let currentSnake = [2, 1, 0];
+  let direction = 1;
+  let score = 0;
+  let speed = 1;
+  let intervalTime = 0;
+
+  // Start/Restart
+  function startGame() {
+    currentSnake.forEach((i) => squares[i].classList.remove('snake'));
+    squares[appleIndex].classList.remove('apple');
+    clearinterval(intervalTime);
+    score = 0;
+
+    // Random apple
+    direction = 1;
+    scoreDisplay.innerText = score;
+    intervalTime = 1000;
+    currentSnale = [2, 1, 0];
+    index = 0;
+    currentSnake.forEach((i) => squares[i].classList.add('snake'));
+    interval = setInterval(moveOutcomes, intervalTime);
+  }
+
+  //   Snake behaviour
+  function moveOutcomes() {
+    // Hit self and borders
+    // Eating apple
+  }
+
+  //   Keyboard binding
+  document.addEventListener('keyup', control);
+
+  function control(e) {
+    squares[index].classList.remove('snake');
+
+    if (e.keyCode === 37) {
+      direction - 1;
+    } else if (e.keyCode === 38) {
+      direction = -width;
+    } else if (e.keyCode === 39) {
+      direction = 1;
+    } else if (e.keyCode === 40) {
+      direction = +width;
+    }
+  }
 });
